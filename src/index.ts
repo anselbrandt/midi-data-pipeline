a100d5815e8a881508fa07ed7042a6513074f118
anselbrandt <ansel@anselbrandt.com>
2024-01-25 03:32:22 -0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { clean } from "./pipeline";
+import fs from "fs/promises";
+import path from "path";
+import { midiToSequenceProto, quantizeNoteSequence } from "./midi";
+import { mkdirs } from "./pipeline";
 
 async function main() {
   const urls = [
@@ -11,7 +14,29 @@ async function main() {
   // const dupes = await getDupes();
   // const deleteList = await getDeleteList(dupes);
   // await deleteDupes(deleteList);
-  await clean();
+  // await clean();
+  const dir = "./midi";
+  const outdir = "./playable";
+  const faildir = "./notplayable";
+  await mkdirs([outdir, faildir]);
+  const files = await fs.readdir(dir);
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const buffer = await fs.readFile(filePath);
+    try {
+      const ns = midiToSequenceProto(buffer);
+      const sequence = quantizeNoteSequence(ns, 4);
+      if (sequence.totalTime > 0) {
+        const outpath = path.join(outdir, file);
+        await fs.writeFile(outpath, buffer);
+      }
+    } catch (error) {
+      if (error) {
+        const failpath = path.join(faildir, file);
+        await fs.writeFile(failpath, buffer);
+      }
+    }
+  }
 }
 
 main().catch((error) => console.log(error));
